b77769c4e21c00d9aea65a730cabcbde5d464d35
Aditya Kumar Singh <94990196+anonymouslyfadeditzme@users.noreply.github.com>
2022-04-28 22:37:00 +0530

    Update Script_LPG_Booking.sql

--- Script_LPG_Booking.sql
+++ Script_LPG_Booking.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE `lpg_booking` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-SELECT * FROM `lpg_booking`.`booking`;
-CREATE TABLE `booking` (
+CREATE TABLE `lpg_booking`.`booking` (
   `ConsumerID` int NOT NULL,
   `BookingDate` varchar(45) DEFAULT NULL,
   `DeliveryDate` varchar(45) DEFAULT NULL,
